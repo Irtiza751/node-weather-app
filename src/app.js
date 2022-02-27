a23d25508d88d0950ccb83e72919101cbc05683a
Muhammad Irtiza <muhammadirtiza@pop-os.localdomain>
2022-02-27 14:57:00 +0500

    Update app.js & add help 4040 pages

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+
+// Initializing app
 const app = express();
 
-// define paths for express config
-const publicPath = path.join(__dirname, "../public");
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../tamplates/partials");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-// setup handlebars engine and views location
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
-// setup static directory to save
-app.use(express.static(publicPath));
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index.hbs", {
@@ -30,10 +32,18 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.get("/weather", (req, res) => {
-  res.send({
-    forecast: "Today is a bueautyfull day",
-    location: "Pakistan",
+app.get("/help", (req, res) => {
+  res.render("help.hbs", {
+    title: "Help",
+    name: "Muhammad Irtiza",
+  });
+});
+
+// 404
+app.get("*", (req, res) => {
+  res.render("404.hbs", {
+    title: "404",
+    name: "Muhammad Irtiza",
   });
 });
 
